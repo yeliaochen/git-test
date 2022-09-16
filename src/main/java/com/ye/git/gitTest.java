dfd426715e6a9014ff5575739e74b5bb393e64a2
yeming <yeming@ye.com>
2022-09-16 11:29:21 +0800

    first commit

---
+++ src/main/java/com/ye/git/gitTest.java
@@ -0,0 +1,11 @@
+package com.ye.git;
+
+/**
+ * @author 叶明伦
+ * @create 2022-09-16 11:25
+ */
+public class gitTest {
+    public static void main(String[] args) {
+        System.out.println("Hello git!");
+    }
+}
